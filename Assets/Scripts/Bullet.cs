e765f42a1d3a3558c6d5eb98b19bc922215b25e6
LibertyA5 <li082460@ucf.edu>
2025-10-30 17:03:38 -0400

    added notes and reviewed zoom part 1
    reviewed zoom and added notes, part 1

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -5,10 +5,17 @@ public class Bullet : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created, not needed
 
     // Update is called once per frame
-  // Update is called once per frame
+    // Update is called once per frame
+
+    //move up and destroy after a certain y value 
+    // float represents speed of bullets 
+    //vector, time, speed multiped 
     void Update()
     {
         transform.Translate(new Vector3(0, 1, 0) * Time.deltaTime * 8f);
+        
+        //destroys at higher than screen limit, which is 6.5 
+        
         if (transform.position.y > 6.5f)
         {
             Destroy(this.gameObject);
--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -3,28 +3,61 @@ using UnityEngine;
 
 public class player : MonoBehaviour
 {
+    //movement
+    //shooting
+    //scope access modifier private or public 
+
     private float playerSpeed;
     private float horizontalInput;
     private float verticalInput;
 
-    private float horizontalScreenLimit = 9.5f;
+    private float horizontalScreenLimit = 9.5f; //f denotes as float
     private float verticalScreenLimit = 6.5f;
 
     public GameObject bulletPrefab;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    // Start is called ONCE before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
           playerSpeed = 6f;
         //This function is called at the start of the game
     }
 
-    // Update is called once per frame
+    // Update is called once per frame THROUGHOUT THE GAME
     void Update()
     {
         //This function is called every frame; 60 frames/second
+        //2 METHODS we are preforming, methods are actions, variables are attributes to actions 
         Movement();
         Shooting();
     }
+
+    //void = output 
+    void Movement()
+    {
+        //Read the input from the player
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        //Move the player
+        transform.Translate(new Vector3(horizontalInput, verticalInput, 0) * Time.deltaTime * playerSpeed);
+        //1,1,0 * 0.1 = 0.1,0.1,0 * 6 speed = 0.6, 0.6, 0 = where player ends up
+
+
+
+        //Player leaves the screen horizontally with x value of transform , greater than or less than negative gets reversed 
+        if (transform.position.x > horizontalScreenLimit || transform.position.x <= -horizontalScreenLimit)
+        {
+            transform.position = new Vector3(transform.position.x * -1, transform.position.y, 0);
+        }
+
+        //Player leaves the screen vertically, with y value of transform 
+        if (transform.position.y > verticalScreenLimit || transform.position.y <= -verticalScreenLimit)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y * -1, 0);
+        }
+    }
+    
      void Shooting()
     {
         //if the player presses the SPACE key, create a projectile
@@ -35,23 +68,6 @@ public class player : MonoBehaviour
         }
     }
 
-    void Movement()
-    {
-        //Read the input from the player
-        horizontalInput = Input.GetAxis("Horizontal");
-        verticalInput = Input.GetAxis("Vertical");
-        //Move the player
-        transform.Translate(new Vector3(horizontalInput, verticalInput, 0) * Time.deltaTime * playerSpeed);
-        //Player leaves the screen horizontally
-        if(transform.position.x > horizontalScreenLimit || transform.position.x <= -horizontalScreenLimit)
-        {
-            transform.position = new Vector3(transform.position.x * -1, transform.position.y, 0);
-        }
-        //Player leaves the screen vertically
-        if(transform.position.y > verticalScreenLimit || transform.position.y <= -verticalScreenLimit)
-        {
-            transform.position = new Vector3(transform.position.x, transform.position.y * -1, 0);
-        }
-    }
+    
 
 }
